91c3cb8ba9ef361db07a11a19e1b40fa41b556f5
rael5 <rael1up@gmail.com>
2025-07-05 22:42:34 -0400

    Primera version funcional GTKBuilder + Glade

--- main.c
+++ main.c
@@ -1,44 +1,70 @@
-#include <gtk/gtk.h>
+﻿#include <gtk/gtk.h>
 
+static GtkBuilder *builder = NULL;
 
-static void on_hello_clicked(GtkWidget *widget, gpointer data) {
-    g_print("confio\n");
+/* Prototipo del callback para cerrar la ventana */
+static void on_main_window_destroy(GtkWidget *widget, gpointer data) {
+    gtk_main_quit();
 }
 
-static void on_quit_clicked(GtkWidget *widget, gpointer data) {
-    gtk_window_close(GTK_WINDOW(data));
+G_MODULE_EXPORT void on_btnInventario_clicked(GtkButton *button, gpointer user_data)
+{
+    GtkStack *stack = GTK_STACK(user_data);
+    // Recupera el widget hijo por su ID de Glade
+    GtkWidget *page = GTK_WIDGET(
+        gtk_builder_get_object(builder, "page_inventario")  // o el ID que le diste
+    );
+
+    // Muestra esa página
+    gtk_stack_set_visible_child(stack, page);
+}
+
+G_MODULE_EXPORT void on_btnProductos_clicked(GtkButton *button, gpointer user_data)
+{
+    GtkStack *stack = GTK_STACK(user_data);
+    // Recupera el widget hijo por su ID de Glade
+    GtkWidget *page = GTK_WIDGET(
+        gtk_builder_get_object(builder, "page_prueba")  // o el ID que le diste
+    );
+
+    // Muestra esa página
+    gtk_stack_set_visible_child(stack, page);
 }
 
 int main(int argc, char *argv[]) {
+    //GtkBuilder *builder;
+    GtkWidget  *window;
+    GError     *error = NULL;
+
+    /* Inicializar GTK */
     gtk_init(&argc, &argv);
 
-    // Crear ventana principal
-    GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title(GTK_WINDOW(window), "Ventana con Dos Botones");
-    gtk_window_set_default_size(GTK_WINDOW(window), 300, 100);
-    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+    /* Crear el GtkBuilder y cargar el archivo .glade */
+    builder = gtk_builder_new();
+    if (!gtk_builder_add_from_file(builder, "interfaz.glade", &error)) {
+        g_printerr("Error al cargar UI: %s\n", error->message);
+        g_clear_error(&error);
+        return 1;
+    }
 
-    // Crear contenedor horizontal
-    GtkWidget *box = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 10);
-    gtk_container_set_border_width(GTK_CONTAINER(box), 10);
-    gtk_container_add(GTK_CONTAINER(window), box);
+    /* Obtener la ventana principal por su ID definido en Glade */
+    window = GTK_WIDGET(gtk_builder_get_object(builder, "ventana"));
+    if (!window) {
+        g_printerr("No se encontró widget 'ventana' en el archivo .glade\n");
+        return 1;
+    }
 
-    // Crear botón 1
-    GtkWidget *btn_hello = gtk_button_new_with_label("Yo soy...");
-    g_signal_connect(btn_hello, "clicked", G_CALLBACK(on_hello_clicked), NULL);
-    gtk_box_pack_start(GTK_BOX(box), btn_hello, TRUE, TRUE, 0);
+    /* Conectar señales definidas en Glade con nuestros callbacks */
+    gtk_builder_connect_signals(builder, NULL);
 
-    // Crear botón 2
-    GtkWidget *btn_quit = gtk_button_new_with_label("Cerrar");
-    g_signal_connect(btn_quit, "clicked", G_CALLBACK(on_quit_clicked), window);
-    gtk_box_pack_start(GTK_BOX(box), btn_quit, TRUE, TRUE, 0);
+    /* Ya no necesitamos el builder */
+    //g_object_unref(builder);
 
-    // Mostrar todo
+    /* Mostrar la ventana y todos sus hijos */
     gtk_widget_show_all(window);
+
+    /* Bucle principal de GTK */
     gtk_main();
 
     return 0;
 }
-
-
-//gcc $(pkg-config --cflags gtk+-3.0) Main.c $(pkg-config --libs gtk+-3.0) -o Main.exe
